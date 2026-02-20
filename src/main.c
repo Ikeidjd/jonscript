3dde4ee0566a2dd7c110ec234ca2681a098358af
Iker-lab441 <iker.pastor@edu.uah.es>
2026-02-20 14:22:29 +0100

    Made filepath relative

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include "lexer.h"
 
 int main() {
-    TokenArray array = lex("C:\\Users\\ikipa\\programming\\jonscript\\res\\main.jon");
+    TokenArray array = lex("res/main.jon");
     for(int i = 0; i < array.length; i++) token_println(array.tokens[i]);
     token_array_destruct(array);
     return 0;
